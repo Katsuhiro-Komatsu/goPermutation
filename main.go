26d0587dce9bf90c0c810deb0a415449f8c1b859
Katsuhiro Komatsu <jazz64k@gmail.com>
2024-05-06 11:52:57 +0900

    桁数を引数で指定するようにした。

--- main.go
+++ main.go
@@ -1,19 +1,29 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"goPerm/permutation"
 )
 
-//import permutation
+func init() {
+
+}
 
 /*
 go言語による順列生成
+
+	使い方：
+	goPerm -n 6		<== 6桁の順列を生成する。
 */
 func main() {
-	const ketasu = 9
+	// 桁数を引数から取得する（指定なければ5桁）
+	var ketasu = flag.Int("n", 5, "順列の桁数")
+	flag.Parse()
+	// 順列を求める
 	perm := new(permutation.Permutation)
-	perm.Calc(ketasu)
+	perm.Calc(*ketasu)
+	// 結果表示
 	for ix := 0; ix < len(perm.Result); ix += 1 {
 		fmt.Println(perm.Result[ix])
 	}
